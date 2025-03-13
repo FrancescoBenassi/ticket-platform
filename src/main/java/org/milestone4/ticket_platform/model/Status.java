c38d59e6d8f96749a644ffe5b3932828d1aedf40
FrancescoBenassi <francybenna77@gmail.com>
2025-03-13 21:34:35 +0100

    feat: added Status class

---
+++ src/main/java/org/milestone4/ticket_platform/model/Status.java
@@ -0,0 +1,49 @@
+package org.milestone4.ticket_platform.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = ("status"))
+public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Status cannot be blank, empty or null")
+    private String status;
+
+    @OneToMany(mappedBy = "status")
+    private List<Ticket> tickets;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Ticket> getTickets() {
+        return this.tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+}
