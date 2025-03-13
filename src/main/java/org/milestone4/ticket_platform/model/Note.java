6c34d3d4dc884e0edee190cff315de82f204f5f0
FrancescoBenassi <francybenna77@gmail.com>
2025-03-13 21:34:16 +0100

    feat: added Note class

---
+++ src/main/java/org/milestone4/ticket_platform/model/Note.java
@@ -0,0 +1,63 @@
+package org.milestone4.ticket_platform.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate dateCreate;
+    @NotBlank(message = "Details cannot be blank, empty or null")
+    @Lob
+    private String details;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    @JsonBackReference
+    private Ticket ticket;
+
+    public LocalDate getDateCreate() {
+        return this.dateCreate;
+    }
+
+    public void setDateCreate(LocalDate dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    public String getDetails() {
+        return this.details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
