68922bf7c6c0b8b60ab9e4bc035e74c04f63359d
FrancescoBenassi <francybenna77@gmail.com>
2025-03-13 21:34:02 +0100

    feat: added Category class

---
+++ src/main/java/org/milestone4/ticket_platform/model/Category.java
@@ -0,0 +1,49 @@
+package org.milestone4.ticket_platform.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Name cannot be blank, empty o null")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Ticket> tickets;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ticket> getTickets() {
+        return this.tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+}
