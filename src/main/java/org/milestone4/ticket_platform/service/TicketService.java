ff5ec6d0041b4e288a8f9e37c42c8cb669810ab4
FrancescoBenassi <francybenna77@gmail.com>
2025-03-13 21:35:13 +0100

    feat: added Ticket Repository and Service

---
+++ src/main/java/org/milestone4/ticket_platform/service/TicketService.java
@@ -0,0 +1,12 @@
+package org.milestone4.ticket_platform.service;
+
+import org.milestone4.ticket_platform.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+}
