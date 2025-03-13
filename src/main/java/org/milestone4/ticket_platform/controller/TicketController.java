05f453d58f9de9476dc1fa47a18f924319cfff85
FrancescoBenassi <francybenna77@gmail.com>
2025-03-13 21:36:00 +0100

    feat: added TicketController

---
+++ src/main/java/org/milestone4/ticket_platform/controller/TicketController.java
@@ -0,0 +1,10 @@
+package org.milestone4.ticket_platform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketController {
+    
+}
