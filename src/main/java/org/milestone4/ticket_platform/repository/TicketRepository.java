2f66f5f452b06adb9ecaa5f96c01b62f938fc556
FrancescoBenassi <francybenna77@gmail.com>
2025-03-14 19:05:21 +0100

    feat: added NoteRepository class and added methods inside TicketRepository and TicketService

--- src/main/java/org/milestone4/ticket_platform/repository/TicketRepository.java
+++ src/main/java/org/milestone4/ticket_platform/repository/TicketRepository.java
@@ -1,8 +1,10 @@
 package org.milestone4.ticket_platform.repository;
 
+import java.util.List;
+
 import org.milestone4.ticket_platform.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket, Integer>{
-    
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    public List<Ticket> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/org/milestone4/ticket_platform/service/TicketService.java
+++ src/main/java/org/milestone4/ticket_platform/service/TicketService.java
@@ -1,5 +1,10 @@
 package org.milestone4.ticket_platform.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.milestone4.ticket_platform.model.Ticket;
+import org.milestone4.ticket_platform.repository.NoteRepository;
 import org.milestone4.ticket_platform.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +13,49 @@ import org.springframework.stereotype.Service;
 public class TicketService {
     @Autowired
     private TicketRepository ticketRepository;
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<Ticket> findByTitle(String title) {
+        return ticketRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public List<Ticket> findAll() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getById(Integer id) {
+        Optional<Ticket> ticket = ticketRepository.findById(id);
+        if (ticket.isEmpty()) {
+            throw new IllegalArgumentException("Ticket not found with id " + id);
+        }
+        return ticket.get();
+    }
+
+    public Optional<Ticket> findById(Integer id) {
+        return ticketRepository.findById(id);
+    }
+
+    public Ticket create(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    public Ticket update(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    /////// DA FINIRE ////////
+
+    // public void delete( Ticket ticket){
+    // for (Note note : ticket.getNotes()) {
+    // noteRepository.delete(note);
+    // }
+    // ticketRepository.delete(ticket);
+    // }
+
+    public void deleteById(Integer id) {
+        ticketRepository.deleteById(id);
+    }
 
 }
