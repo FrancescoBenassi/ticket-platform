4d89545a01369a6a2a83fafd69fe94e55cf79f61
FrancescoBenassi <francybenna77@gmail.com>
2025-03-15 17:40:45 +0100

    feat: added methods inside UserRepository and UserService

--- src/main/java/org/milestone4/ticket_platform/repository/UserRepository.java
+++ src/main/java/org/milestone4/ticket_platform/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.milestone4.ticket_platform.repository;
 
+import java.util.List;
+
 import org.milestone4.ticket_platform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public List<User> findByUsernameContainingIgnoreCase(String username);
 }
--- src/main/java/org/milestone4/ticket_platform/service/UserService.java
+++ src/main/java/org/milestone4/ticket_platform/service/UserService.java
@@ -1,5 +1,10 @@
 package org.milestone4.ticket_platform.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.milestone4.ticket_platform.model.User;
 import org.milestone4.ticket_platform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +13,30 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+     public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public List<User> findByUsername(String username) {
+        return userRepository.findByUsernameContainingIgnoreCase(username);
+    }
+
+    public User getById(Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("User not found with id " + id);
+        }
+        return user.get();
+    }
+
+    public List<User> isAvailableOperator(List<User> users){
+        List<User> userAttempt= new ArrayList<User>();
+        for (User user : users){
+            if(user.isIsAvailable()){
+                userAttempt.add(user);
+            }
+        }
+        return userAttempt;
+    }
 }
