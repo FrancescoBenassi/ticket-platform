4411440a8d1ca76ae1e517b5577a0a782ffc6e16
FrancescoBenassi <francybenna77@gmail.com>
2025-03-16 15:56:36 +0100

    feat StatusRepository: added method

--- src/main/java/org/milestone4/ticket_platform/repository/StatusRepository.java
+++ src/main/java/org/milestone4/ticket_platform/repository/StatusRepository.java
@@ -1,8 +1,10 @@
 package org.milestone4.ticket_platform.repository;
 
+import java.util.List;
+
 import org.milestone4.ticket_platform.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<Status, Integer> {
-    
+    public List<Status> findByNameContainingIgnoreCase(String name);
 }
