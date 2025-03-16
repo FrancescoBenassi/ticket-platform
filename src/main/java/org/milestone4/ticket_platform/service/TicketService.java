0048fea3d742fb0a4494f1527d61c20b62668e24
FrancescoBenassi <francybenna77@gmail.com>
2025-03-16 12:57:02 +0100

    feat: added methods inside TicketService

--- src/main/java/org/milestone4/ticket_platform/service/TicketService.java
+++ src/main/java/org/milestone4/ticket_platform/service/TicketService.java
@@ -1,21 +1,33 @@
 package org.milestone4.ticket_platform.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.milestone4.ticket_platform.model.Note;
 import org.milestone4.ticket_platform.model.Ticket;
+import org.milestone4.ticket_platform.model.User;
+import org.milestone4.ticket_platform.repository.CategoryRepository;
 import org.milestone4.ticket_platform.repository.NoteRepository;
+import org.milestone4.ticket_platform.repository.StatusRepository;
 import org.milestone4.ticket_platform.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketService {
+
     @Autowired
     private TicketRepository ticketRepository;
 
     @Autowired
     private NoteRepository noteRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired 
+    private StatusRepository statusRepository;
 
     public List<Ticket> findByTitle(String title) {
         return ticketRepository.findByTitleContainingIgnoreCase(title);
@@ -37,11 +49,35 @@ public class TicketService {
         return ticketRepository.findById(id);
     }
 
+    public List<Ticket> isAvailableTicket(List<Ticket> tickets){
+        List<Ticket> ticketsAttempt= new ArrayList<Ticket>();
+        for (Ticket ticket : tickets){
+            if(!(ticket.getCategory() == null)){
+                ticketsAttempt.add(ticket);
+            }
+        }
+        return ticketsAttempt;
+    }
+
     public Ticket create(Ticket ticket) {
+        if (ticket.getNotes() != null && !ticket.getNotes().isEmpty()) {
+            for (Note note : ticket.getNotes()) {
+                noteRepository.save(note);
+            }
+        }
+        statusRepository.save(ticket.getStatus());
+        categoryRepository.save(ticket.getCategory());
         return ticketRepository.save(ticket);
     }
 
     public Ticket update(Ticket ticket) {
+        if (ticket.getNotes() != null && !ticket.getNotes().isEmpty()) {
+            for (Note note : ticket.getNotes()) {
+                noteRepository.save(note);
+            }
+        }
+        statusRepository.save(ticket.getStatus());
+        categoryRepository.save(ticket.getCategory());
         return ticketRepository.save(ticket);
     }
 
@@ -54,7 +90,7 @@ public class TicketService {
     // ticketRepository.delete(ticket);
     // }
 
-    public void delete(Ticket ticket){
+    public void delete(Ticket ticket) {
         ticketRepository.delete(ticket);
     }
 
