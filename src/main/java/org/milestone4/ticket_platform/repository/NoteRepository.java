72650383c6959f5a83b15bcd909a0b7e40b77ae5
FrancescoBenassi <francybenna77@gmail.com>
2025-03-16 15:56:05 +0100

    feat NoteRepository: added method

--- src/main/java/org/milestone4/ticket_platform/repository/NoteRepository.java
+++ src/main/java/org/milestone4/ticket_platform/repository/NoteRepository.java
@@ -1,8 +1,10 @@
 package org.milestone4.ticket_platform.repository;
 
+import java.util.List;
+
 import org.milestone4.ticket_platform.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-
+    public List<Note> findByTitleContainingIgnoreCase(String title);
 }
