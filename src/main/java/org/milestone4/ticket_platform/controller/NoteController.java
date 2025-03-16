9a27da6002611df44679ea86c00492bdb7ed84fe
FrancescoBenassi <francybenna77@gmail.com>
2025-03-16 15:54:03 +0100

    feat: added NoteController

---
+++ src/main/java/org/milestone4/ticket_platform/controller/NoteController.java
@@ -0,0 +1,75 @@
+package org.milestone4.ticket_platform.controller;
+
+import java.time.LocalDateTime;
+
+import org.milestone4.ticket_platform.model.Note;
+import org.milestone4.ticket_platform.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/notes")
+public class NoteController {
+
+
+    //////// DA FINIRE //////
+
+    @Autowired
+    private NoteService noteService;
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("note") Note noteForm,
+            BindingResult bindingResult, Model model,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("create", true);
+            return "notes/create-or-edit";
+        }
+        noteForm.setCreationDate(LocalDateTime.now());
+        noteForm.setUpdatedDate(LocalDateTime.now());
+        noteService.create(noteForm);
+        redirectAttributes.addFlashAttribute("message", "A new note has been created");
+        redirectAttributes.addFlashAttribute("alert", "alert-primary");
+        return "redirect:/notes";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("note", noteService.getById(id));
+        return "notes/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("note") Note noteForm,
+            BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "notes/create-or-edit";
+        }
+
+        noteService.update(noteForm);
+        redirectAttributes.addFlashAttribute("message", "A new note has been updated");
+        redirectAttributes.addFlashAttribute("alert", "alert-success");
+        return "redirect:/notes";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        noteService.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", "A note has been deleted");
+        redirectAttributes.addFlashAttribute("alert", "alert-danger");
+        return "redirect:/notes";
+    }
+
+}
+
