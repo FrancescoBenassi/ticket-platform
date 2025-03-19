bdf4dd1c4a1e7c2189412d75d211b7703d403e22
FrancescoBenassi <francybenna77@gmail.com>
2025-03-19 21:53:16 +0100

    feat UserRepository: fixed

--- src/main/java/org/milestone4/ticket_platform/repository/UserRepository.java
+++ src/main/java/org/milestone4/ticket_platform/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package org.milestone4.ticket_platform.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.milestone4.ticket_platform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public List<User> findByUsernameContainingIgnoreCase(String username);
+    public Optional<User> findByUsername(String username);
 }
