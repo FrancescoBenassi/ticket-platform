4bea70a5c26742cb62c22cf79bb69c7ee2512709
FrancescoBenassi <francybenna77@gmail.com>
2025-03-21 23:24:50 +0100

    feat Role: added ToString

--- src/main/java/org/milestone4/ticket_platform/model/Role.java
+++ src/main/java/org/milestone4/ticket_platform/model/Role.java
@@ -50,4 +50,10 @@ public class Role {
     public void setUsers(Set<User> users) {
         this.users = users;
     }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
 }
