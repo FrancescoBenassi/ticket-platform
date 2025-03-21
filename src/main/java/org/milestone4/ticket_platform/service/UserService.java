f547a040c7908e9b59ebbf2926fc490a9f269d88
FrancescoBenassi <francybenna77@gmail.com>
2025-03-21 23:29:17 +0100

    feat UserService: added methods

--- src/main/java/org/milestone4/ticket_platform/service/UserService.java
+++ src/main/java/org/milestone4/ticket_platform/service/UserService.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.milestone4.ticket_platform.model.User;
 import org.milestone4.ticket_platform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,30 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-     public List<User> findAll() {
+    @Autowired
+    private TicketService ticketService;
+
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public List<User> findByUsername(String username) {
-        return userRepository.findByUsernameContainingIgnoreCase(username);
+    public User create(User user) {
+        if(user.isIsAdmin()){
+           user.setIsAvailable(false);
+        }
+        user.setIsAvailable(true);
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        if(user.isIsAdmin()){
+            user.setIsAvailable(false);
+         }
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 
     public User getById(Integer id) {
@@ -30,13 +50,21 @@ public class UserService {
         return user.get();
     }
 
-    public List<User> isAvailableOperator(List<User> users){
-        List<User> userAttempt= new ArrayList<User>();
-        for (User user : users){
-            if(user.isIsAvailable()){
-                userAttempt.add(user);
-            }
+    public List<User> isAvailableOperator(List<User> users) {
+        List<User> userAttempt = new ArrayList<User>();
+        for (User user : users) {
+                if (user.isIsAvailable() && !user.isIsAdmin()) {
+                    userAttempt.add(user);
+                }
         }
         return userAttempt;
+    }
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found, with username " + username));
     }
 }
