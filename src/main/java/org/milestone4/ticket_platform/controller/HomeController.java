0f61797cd4f18aa08c350f14ac739ea67e05063e
FrancescoBenassi <francybenna77@gmail.com>
2025-03-23 15:09:38 +0100

    feat HomeController: added tickets inside controller

--- src/main/java/org/milestone4/ticket_platform/controller/HomeController.java
+++ src/main/java/org/milestone4/ticket_platform/controller/HomeController.java
@@ -1,5 +1,6 @@
 package org.milestone4.ticket_platform.controller;
 
+import org.milestone4.ticket_platform.service.TicketService;
 import org.milestone4.ticket_platform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -15,9 +16,15 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TicketService ticketService;
+
     @GetMapping
     public String index(Model model, Authentication authentication) {
             model.addAttribute("user", userService.getCurrentUser());
+            if (userService.getCurrentUser().getIsAdmin()) {
+                model.addAttribute("tickets", ticketService.findAll());
+            }
         return "home/index";
     }
 }
